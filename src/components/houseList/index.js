97b2885a395e63d8cc12352adec5310e068e05a7
rokkoo <alfonsolasaguirre@gmail.com>
2020-01-03 16:18:19 +0100

    Scroll view for rooms list

--- src/components/houseList/index.js
+++ src/components/houseList/index.js
@@ -2,7 +2,12 @@
 
 import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {SafeAreaView, Dimensions, TouchableOpacity} from 'react-native';
+import {
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
 import Icon from 'react-native-vector-icons/AntDesign';
 
@@ -26,20 +31,22 @@ const HouseList = ({navigation}) => {
 
   const renderItem = ({item}) => {
     return (
-      <ItemWrapper
-        style={{
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 1,
-          },
-          shadowOpacity: 0.2,
-          shadowRadius: 1.41,
+      <TouchableOpacity onPress={nav} activeOpacity={1}>
+        <ItemWrapper
+          style={{
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 0.2,
+            shadowRadius: 1.41,
 
-          elevation: 2,
-        }}>
-        <Image source={{uri: item}} />
-      </ItemWrapper>
+            elevation: 2,
+          }}>
+          <Image source={{uri: item}} />
+        </ItemWrapper>
+      </TouchableOpacity>
     );
   };
 
@@ -52,100 +59,62 @@ const HouseList = ({navigation}) => {
     dispatch({type: 'CHANGE_PAGINATION', data});
   };
 
-  const RenderPagination = ({entries}) => {
-    return (
-      <Pagination
-        dotsLength={entries.pictures.length}
-        activeDotIndex={entries.current_pagination}
-        containerStyle={{
-          height: 65,
-          backgroundColor: 'red',
-        }}
-        dotStyle={{
-          borderRadius: 5,
-        }}
-        inactiveDotStyle={
-          {
-            // Define styles for inactive dots here
-          }
-        }
-        inactiveDotOpacity={0.4}
-        inactiveDotScale={0.6}
-      />
-    );
-  };
-
   return (
     <SafeAreaView>
-      {Object.entries(houses).map(([index, data]) => {
-        return (
-          <PostWrapper key={index}>
-            <Carousel
-              data={data.pictures}
-              renderItem={renderItem}
-              sliderWidth={screenWidth}
-              itemWidth={screenWidth / 3}
-              layout={'default'}
-              activeSlideAlignment={'start'}
-              containerCustomStyle={{paddingLeft: 10}}
-              onSnapToItem={pos => handlePagination(pos, index)}
-            />
-            <Block style={{height: 10}}>
-              <Pagination
-                dotsLength={data.pictures.length}
-                activeDotIndex={data.current_pagination}
-                containerStyle={{
-                  height: 65,
-                  paddingVertical: 0,
-                  marginVertical: -15, // can be perform
-                }}
-                dotStyle={{
-                  borderRadius: 5,
-                  marginHorizontal: 8,
-                  marginVertical: 0,
-                  backgroundColor: '#636e72',
-                }}
-                inactiveDotStyle={
-                  {
-                    // Define styles for inactive dots here
-                  }
-                }
-                inactiveDotOpacity={0.4}
-                inactiveDotScale={0.6}
+      <ScrollView>
+        {Object.entries(houses).map(([index, data]) => {
+          return (
+            <PostWrapper key={index}>
+              <Carousel
+                data={data.pictures}
+                renderItem={renderItem}
+                sliderWidth={screenWidth}
+                itemWidth={screenWidth / 3}
+                layout={'default'}
+                activeSlideAlignment={'start'}
+                containerCustomStyle={{paddingLeft: 10}}
+                onSnapToItem={pos => handlePagination(pos, index)}
               />
-            </Block>
-            <PostFooter>
-              <TouchableOpacity onPress={nav}>
-                <Block
-                  style={{
-                    backgroundColor: '#2ecc71',
-                    borderRadius: 6,
-                    padding: 7,
-                  }}>
-                  <Block
-                    style={{
-                      flexDirection: 'row',
-                      color: '#34495e',
-                      justifyContent: 'space-between',
-                    }}>
-                    <Text>{data.user}</Text>
-                    <Text style={{paddingLeft: 5}}>
-                      <Icon name="arrowright" size={20} />
-                    </Text>
-                  </Block>
+              <Block style={{height: 10}}>
+                <Pagination
+                  dotsLength={data.pictures.length}
+                  activeDotIndex={data.current_pagination}
+                  containerStyle={{
+                    height: 65,
+                    paddingVertical: 0,
+                    marginVertical: -15, // can be perform
+                  }}
+                  dotStyle={{
+                    borderRadius: 5,
+                    marginHorizontal: 8,
+                    marginVertical: 0,
+                    backgroundColor: '#636e72',
+                  }}
+                  inactiveDotStyle={
+                    {
+                      // Define styles for inactive dots here
+                    }
+                  }
+                  inactiveDotOpacity={0.4}
+                  inactiveDotScale={0.6}
+                />
+              </Block>
+              <PostFooter>
+                <Block>
+                  <Text style={{}}>{data.user}</Text>
                   <Text big>{data.location_name}</Text>
                 </Block>
-              </TouchableOpacity>
-              <Icon
-                name={data.liked ? 'heart' : 'hearto'}
-                size={20}
-                color={data.liked ? '#6c5ce7' : '#636e72'}
-                onPress={() => handleLike(index)}
-              />
-            </PostFooter>
-          </PostWrapper>
-        );
-      })}
+                <Icon
+                  name={data.liked ? 'heart' : 'hearto'}
+                  size={20}
+                  color={data.liked ? '#6c5ce7' : '#636e72'}
+                  onPress={() => handleLike(index)}
+                />
+              </PostFooter>
+            </PostWrapper>
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 };
